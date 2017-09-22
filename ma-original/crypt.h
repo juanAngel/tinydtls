dc56691f0d37a0d9afab3f015dd234e88a91a035
Gaire, Raj (Data61, Acton) <Raj.Gaire@csiro.au>
2017-09-22 12:35:59 +1000

    merged crypt and encode

--- ma-original/crypt.h
+++ ma-original/crypt.h
@@ -112,3 +112,15 @@ void printSISSample(char* msg, struct PubKey* pubk, struct Cipher* D);
 
 
 
+/* from encode.h */
+int decode(unsigned char* buf, unsigned long buf_len, mpz_t res);
+int encode(unsigned char* buf, unsigned long buf_len, mpz_t res);
+
+int encodeG(unsigned char* buf, unsigned long buf_len, mpz_t* res);
+int decodeG(unsigned char* buf, unsigned long buf_len, mpz_t* res, int debug);
+
+
+unsigned char* randBytes(int buflen);
+unsigned char* randomhex(int len);
+
+
